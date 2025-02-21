064c0a6c4106965f5e7e2ee6cbcf50d2cfc62206
Elizaveta Lozhkina <elizaveta.lozhkina@epfl.ch>
2025-02-21 15:04:06 +0100

    final fix bug resolved

--- Atomization_energy/Atomization_calc.py
+++ Atomization_energy/Atomization_calc.py
@@ -1,135 +1,66 @@
 #!/usr/bin/env python3
 
-import glob
 import os
-import re
 import numpy as np
 import qstack as qs
 
-logfile = "/home/student5/lise/MasterProject_SPAHM-ENN/opt/${molname}/${molname}.log"
 outputfile = "/home/student5/lise/MasterProject_SPAHM-ENN/Atomization_energy/Atomization_energies.txt"
 ZPEfile = "ZPE.txt"
 E0_file = "E_0.txt"
 mol_list = "mol_list.txt"
-geom_opt_xyz = "/home/student5/lise/MasterProject_SPAHM-ENN/geom_optimized_xyz/${molname}.xyz"
+E_CNOSH_file = "E_CNOSH.txt"
 
-data = np.loadtxt(outputfile, dtype=str) ### create a dictionnary {Z  : E0}
-elements = data[:, 0]  
-energies = data[:, 1].astype(float)  
-E0_elements = dict(zip(elements, energies))
-print(E0_elements)
+E0_elements = {}
+if os.path.exists(E_CNOSH_file):
+    with open(E_CNOSH_file, 'r') as f:
+        for line in f:
+            parts = line.split()
+            if len(parts) == 2:
+                element, energy = parts
+                E0_elements[element] = float(energy)
+else:
+    print(f"Le fichier {E_CNOSH_file} est introuvable. Assurez-vous qu'il existe.")
 
-### use qstack to convert xyz to Mole to calcule the number of elements qstack.compound.xyz_to_mol()
+print("Dictionnaire des énergies des atomes (élément : énergie):", E0_elements)
+
+def get_zpe(index):
+    with open(ZPEfile, 'r') as f:
+        zpe_lines = f.readlines()
+    return float(zpe_lines[index].strip())
+
+def get_e0(index):
+    with open(E0_file, 'r') as f:
+        e0_lines = f.readlines()
+    return float(e0_lines[index].strip())
 
 def calculate_x_energies(molname):
-    mol = qs.compound.xyz_to_mol(f"/home/student5/lise/MasterProject_SPAHM-ENN/geom_optimized_xyz/{molname}.xyz",
-                                 basis='Def2SVP', charge=1, spin=2, ignore=False, unit='ANG', ecp=None)
-    mol_elements = mol.elements  
-    energy_sum = 0.0
-    for element in mol_elements:
-        energy_sum += E0_elements.get(element, 0) 
+    mol_path = f"/home/student5/lise/MasterProject_SPAHM-ENN/geom_optimized_xyz/{molname}.xyz"
+
+    if not os.path.exists(mol_path):
+        print(f"Fichier {mol_path} introuvable. Ignoré.")
+        return 0.0
+
+    mol = qs.compound.xyz_to_mol(mol_path, basis='Def2SVP', charge=1, spin=1, ignore=False, unit='ANG', ecp=None)
+    mol_elements = mol.elements
+    energy_sum = sum(E0_elements.get(element, 0) for element in mol_elements)
     return energy_sum
 
-np.savetxt('', namelist)
-
-### create a function to calculate an atomization energy for only one molecule
-### use a dictionnary E0_elements energie --> sum+=dict(e) with energies in mol.elements
-### calculate an atomization energy and return
-
-resultats = [] ###create a list of results
-for fichier in glob.glob(os.path.join(dossier_mol, "*.inp")):
-
-    atomes = compter_atomes_pyscf(fichier)
-    energie_totale = sum(atomes[element] * energies_atomiques.get(element, 0) for element in atomes)
-    resultats.append('{}: mol_001 = {}\n'.format(fichier, energie_totale))
-
-return
-
-with open(outputfile, "w") as f:
-    f.writelines(resultats)
-
-print(outputfile)
-                                                                                                                                                   60,17         Bot
-def calculate_atomization_energy(molname):
-    mol = qs.compound.xyz_to_mol(f"/home/student5/lise/MasterProject_SPAHM-ENN/geom_optimized_xyz/{molname}.xyz", 
-                                 basis='Def2SVP', charge=1, spin=2, ignore=False, unit='ANG', ecp=None)
-    mol_elements = mol.elements  
-    energy_sum = 0.0
-    for element in mol_elements:
-        energy_sum += E0_elements.get(element, 0)  
-    return energy_sum
-resultats = []  
-dossier_mol = "/home/student5/lise/MasterProject_SPAHM-ENN"
-for fichier in glob.glob(os.path.join(dossier_mol, "*.inp")):
-    molname = os.path.basename(fichier).replace(".inp", "") 
-    energie_totale = calculate_atomization_energy(molname)
-    resultats.append(f'{molname}: Atomization energy = {energie_totale:.8f} kcal/mol\n')
-with open(outputfile, "w") as f:
-    f.writelines(resultats)
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def calculate_atomization_energy(molname, index):
+    mol_energy = calculate_x_energies(molname)
+    zpe = get_zpe(index)
+    e0 = get_e0(index)
+    atomization_energy = mol_energy - zpe - e0
+    return atomization_energy
+
+with open(mol_list, 'r') as f:
+    molnames = [line.strip() for line in f.readlines()]
+
+resultats = []
+for index, molname in enumerate(molnames):
+    atomization_energy = calculate_atomization_energy(molname, index)
+    print(f"Calcul de l'énergie d'atomisation pour {molname}: {atomization_energy}")
+    resultats.append([molname, atomization_energy])
+
+np.savetxt(outputfile, resultats, fmt="%s", delimiter="\t", header="Molecule\tAtomization_Energy (kcal/mol)", comments='')
+
+print(f"Les énergies d'atomisation ont été sauvegardées dans {outputfile}")
