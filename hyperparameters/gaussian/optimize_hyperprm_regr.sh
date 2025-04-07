8a91bb2dc77c3c498146ace515465dc3113bfc11
Elizaveta Lozhkina <elizaveta.lozhkina@epfl.ch>
2025-04-07 12:59:21 +0200

    IUpdate the codes

--- hyperparameters/gaussian/optimize_hyperprm_regr.sh
+++ hyperparameters/gaussian/optimize_hyperprm_regr.sh
@@ -8,7 +8,7 @@ for atom in "${atoms[@]}"; do
     for rep in "${reps[@]}"; do
         for seed in "${seeds[@]}"; do
             JOBNAME="${atom}-${rep}-${seed}_QM7_hyperprm"
-            sbatch --job-name=$JOBNAME --mem=60GB -n 1 -c 2 --wrap="conda run -n new-q-stack --live-stream python hyperparameters_regression.py $atom $rep $seed"
+            sbatch --job-name=$JOBNAME --mem=60GB -n 1 -c 2 --wrap="conda run -n new-q-stack --live-stream python hyperparam_eta_sigma_given.py $atom $rep $seed"
         done
     done
 done
