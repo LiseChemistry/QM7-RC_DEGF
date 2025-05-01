7ea7b0682709fba710ed77b7fa8a0a5b5c20bc61
Elizaveta Lozhkina <elizaveta.lozhkina@epfl.ch>
2025-05-01 11:28:41 +0200

    Import molecular representaion

---
+++ spahm/b_spahm/import_b_spahm_results_to_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+with open('data_b_spahm.csv', 'w', encoding='utf-8') as f:
+    with open('good_logs_list.txt') as f_list:
+        for line in f_list:
+            fname = 'results/' + line.strip() + '.npy'
+            if fname:
+                data = np.load(fname)
+                summed = np.sum(data, axis=0, keepdims=True) 
+
+                f.write(' '.join(map(str, summed.flatten())) + '\n')
+
