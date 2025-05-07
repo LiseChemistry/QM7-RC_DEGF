047d10f7b66030cd18cb9cd5d91106478ae347b3
Elizaveta Lozhkina <elizaveta.lozhkina@epfl.ch>
2025-05-07 18:04:01 +0200

    Update a code to cerate a vector for e_spahm

--- spahm/e_spahm/create_vectors.py
+++ spahm/e_spahm/create_vectors.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
 import os
-import numpy as np
 import shutil
-import ast
 
 source_dir = 'results'
 target_dir = 'representations'
@@ -14,29 +12,37 @@ for filename in os.listdir(source_dir):
     shutil.copy(src, dst)
 
 def complete_vector(vec, target_length=33):
+    """
+    Complete a vector in the form of a string with zeros until it reaches the target length.
+    """
     if len(vec) < target_length:
-        vec += [0.0] * (target_length - len(vec))  
+        vec += ['0.0'] * (target_length - len(vec))  
     return vec
 
 def process_file(file_path):
+    """
+    Complete a vector as a string with zeros until it reaches the target length.
+    """
     with open(file_path, 'r') as file:
         lines = file.read()
+
         lines = lines.replace('] [', '],[')
 
         data = []
         for line in lines.strip().split("\n"):
-            vec = list(map(float, line.split()))
+            vec = line.split()
             data.append(vec)
 
     vector1 = complete_vector(data[0])
     vector2 = complete_vector(data[1])
 
     with open(file_path, 'w') as f:
-        f.write(" ".join(map(str, vector1)) + "\n")
-        f.write(" ".join(map(str, vector2)) + "\n")
+        f.write(" ".join(vector1) + "\n")
+        f.write(" ".join(vector2) + "\n")
 
 for filename in os.listdir(target_dir):
     file_path = os.path.join(target_dir, filename)
     process_file(file_path)
 
 print("Finished.")
+
